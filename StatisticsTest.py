0490a80c18a9170345d713f5445dfda471e9d194
Maria Arevalo <mariaisabelarevalo@hotmail.com>
2019-09-29 17:16:36 -0500

    Iteración 1, Paso 4: se crea el test para cuando la cadena tiene mas de dos números (Rojo)

--- StatisticsTest.py
+++ StatisticsTest.py
@@ -13,4 +13,8 @@ class StatisticsTest(TestCase):
     def test_values_twoNumbers(self):
         self.assertEqual(Statistics().values("1,2"), [2], "Cadena Con dos números")
 
+    def test_values_NNumbers(self):
+        self.assertEqual(Statistics().values("1,2,6,2"), [4], "Cadena Con mas de dos números")
+        self.assertEqual(Statistics().values("1,2,6,2,5,2,6"), [7], "Cadena Con mas de dos números")
+
 
