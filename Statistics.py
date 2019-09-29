62a992b22bd0819e809e05079f569c25c0f1a786
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 17:29:04 -0500

    Iteración 2, Paso 1: se modifica el método para cuando la cadena es vacía y, además, devuelva el mínimo (Verde)

--- Statistics.py
+++ Statistics.py
@@ -1,7 +1,7 @@
 class Statistics:
     def values(self, string):
         if string == "":
-            return [0]
+            return [0, 0]
         elif ',' in string:
             cadena = string.split(",")
             return [len(cadena)]
