f534475d2088a640543865c821a962656b45854a
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 17:26:54 -0500

    Iteración 2, Paso 1: se crea el test para cuando la cadena tiene cadena vacía (Rojo)

--- StatisticsTest.py
+++ StatisticsTest.py
@@ -4,7 +4,7 @@ from Statistics import Statistics
 
 class StatisticsTest(TestCase):
     def test_values(self):
-        self.assertEqual(Statistics().values(""), [0], "Cadena Vacía")
+        self.assertEqual(Statistics().values(""), [0, 0], "Cadena Vacía")
 
     def test_values_oneNumber(self):
         self.assertEqual(Statistics().values("1"), [1], "Cadena Con un número")
@@ -16,5 +16,4 @@ class StatisticsTest(TestCase):
     def test_values_NNumbers(self):
         self.assertEqual(Statistics().values("1,2,6,2"), [4], "Cadena Con mas de dos números")
         self.assertEqual(Statistics().values("1,2,6,2,5,2,6"), [7], "Cadena Con mas de dos números")
-
 
