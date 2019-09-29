e4206045a10bd0aaee03dca7b4cea8cf2e68382c
Maria Arevalo <mariaisabelarevalo@hotmail.com>
2019-09-29 17:09:05 -0500

    Iteración 1, Paso 2: se crea el test para cuando la cadena tiene un número (Rojo)

--- StatisticsTest.py
+++ StatisticsTest.py
@@ -4,3 +4,8 @@ from Statistics import Statistics
 class StatisticsTest(TestCase):
     def test_values(self):
         self.assertEqual(Statistics().values(""), [0], "Cadena Vacía")
+
+    def test_values_oneNumber(self):
+        self.assertEqual(Statistics().values("1"), [1], "Cadena Con un número")
+        self.assertEqual(Statistics().values("5"), [1], "Cadena Con un número")
+
