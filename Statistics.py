e8f9a79fb319087f8c5251bc2f186dec3f6d9ca7
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 17:38:28 -0500

    Iteración 2, Paso 3: se modifica el método para cuando la cadena contiene dos números y, además, devuelva el mínimo (Verde)

--- Statistics.py
+++ Statistics.py
@@ -4,6 +4,6 @@ class Statistics:
             return [0, 0]
         elif ',' in string:
             cadena = string.split(",")
-            return [len(cadena)]
+            return [len(cadena), min(int(string[0]), int(string[2]))]
         else:
             return [1, int(string)]
