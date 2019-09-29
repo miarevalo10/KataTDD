3c5cfe8954498af115d74127307306285376edbb
Armando Acosta Guerrero <darman.89@gmail.com>
2019-09-29 18:00:15 -0500

    Iteración 4, Paso 2: se modifica el test para cuando la cadena contiene un número (Rojo)

--- StatisticsTest.py
+++ StatisticsTest.py
@@ -7,8 +7,8 @@ class StatisticsTest(TestCase):
         self.assertEqual(Statistics().values(""), [0, 0, 0, 0], "Cadena Vacía")
 
     def test_values_oneNumber(self):
-        self.assertEqual(Statistics().values("1"), [1, 1, 1], "Cadena Con un número")
-        self.assertEqual(Statistics().values("5"), [1, 5, 5], "Cadena Con un número")
+        self.assertEqual(Statistics().values("1"), [1, 1, 1, 1], "Cadena Con un número")
+        self.assertEqual(Statistics().values("5"), [1, 5, 5, 5], "Cadena Con un número")
 
     def test_values_twoNumbers(self):
         self.assertEqual(Statistics().values("1,2"), [2, 1, 2], "Cadena Con dos números")
